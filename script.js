3d9bc826e1908beaa80ae4f5bf0070152b090516
Atmani Chouaib <atmanichouaib22@gmail.com>
2023-08-14 12:13:52 +0100

    refactored all the logic source code added some basic styling

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
-//random Choice generator 
-const choicesArray = ["rock" , "paper", "scissor"] ; 
+
 let computerScore = 0 , playerScore = 0 ; 
+//random Choice generator 
 function computerChoice(){ 
+    const choicesArray = ["rock" , "paper", "scissor"] ; 
     return choicesArray[Math.floor(Math.random()* choicesArray.length)];
 }
  
-//all possible cases 
+//Game Logic
 function playRound(playerSelect, computerSelect){ 
      if(playerSelect === "") {
         console.log("you entered nothing");
         return ; 
     }else if(playerSelect=== computerSelect){
-        return `Tie Game`; 
+        return `Tie Game You!! you ${playerScore} || computer ${computerScore}`; 
     }else if (playerSelect === "rock" && computerSelect ==="scissor"){
         playerScore++ ; 
-        return `Player won` ; 
+        return `Player won!! you ${playerScore} || computer ${computerScore}` ; 
     }else if (playerSelect === "paper" && computerSelect ==="rock"){
         playerScore++ ;
-        return `Player won` ; 
+        return `Player won!! you ${playerScore} || computer ${computerScore}` ; 
     }else if (playerSelect === "scissor" && computerSelect ==="paper"){
         playerScore++ ;
-        return `Player won` ; 
+        return `Player won!! you ${playerScore} || computer ${computerScore}` ; 
     }else if (playerSelect === "paper" && computerSelect ==="scissor"){
         computerScore++ ;
-        return `Computer won` ; 
+        return `Computer won!! you ${playerScore} || computer ${computerScore}` ; 
     }else if (playerSelect === "scissor" && computerSelect ==="rock"){
         computerScore++ ;
-        return `Computer won` ; 
+        return `Computer won you ${playerScore} || computer ${computerScore}` ; 
     }else if (playerSelect === "rock" && computerSelect ==="paper"){
         computerScore++ ;
-        return `Computer won` ; 
+        return `Computer won you ${playerScore} || computer ${computerScore}` ; 
     }else{
         return ; 
     }
 }
-function itIsFive(event){
-    event.preventDefault(); 
+
+const btn = document.querySelectorAll("button"); 
+const output = document.querySelector('.output'); 
+
+function endGame(){
+    console.log("game ended"); 
+    btn.forEach(button => {
+        button.removeEventListener("click", gameFunc); 
+    })
 }
-const buttonSelect = document.querySelectorAll("button"); 
-buttonSelect.forEach((button) =>{
-    button.addEventListener("click", (e)=> {
-        let computerSelection = computerChoice() ; 
-        let playerSelection = button.getAttribute("data-value"); 
-        let display = document.createElement("p"); 
-        display.textContent = playRound(playerSelection , computerSelection); 
-        const sec = document.querySelector("body"); 
-        sec.setAttribute("class" , "displayStyle"); 
-        sec.appendChild(display);
-        if(playerScore===5 || computerScore===5){
-            itIsFive(e); 
-        }  
-    }) 
-})
+
+function gameFunc(e){ 
+    let playerSelection = e.target.getAttribute("data-value"); 
+    let computerSelection = computerChoice(); 
+    let resText = document.createElement("p"); //created a p element
+    resText.textContent = playRound(playerSelection,computerSelection); //resault inside p
+    output.append(resText);  //appending resault to output container
+    //checking resaults
+    if(playerScore===5 || computerScore===5){
+        endGame(); 
+        return;
+    } 
+};
+
+btn.forEach(button => {
+    button.addEventListener("click", gameFunc)
+});
  
